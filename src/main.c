aeb446977ce8fcf330f26a137303fd18b919be4b
Gideon Fisher <gidafish@gmail.com>
2026-03-07 11:16:45 -0600

    Added a window

--- src/main.c
+++ src/main.c
@@ -1,8 +1,29 @@
-// Placeholder for now...
-
+#include <GLFW/glfw3.h>
 #include <stdio.h>
 
 int main() {
-    printf("Starting gentest...");
+    if (!glfwInit()) {
+        printf("Failed to initialize GLFW\n");
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "gentest", NULL, NULL);
+
+    if (!window) {
+        printf("Failed to create window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
